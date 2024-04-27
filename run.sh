64b1ff66e60fecac3e13602b2658cc4dc136bb20
Rodger Xiang <rodxiang2@gmail.com>
2024-04-27 20:03:13 +1000

    added in the other env to test to run script

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=30G
 #SBATCH --job-name=CURL
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --partition=gpu_cuda
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=gpu:a100
 #SBATCH --account=a_lead
 #SBATCH -o curl.out
 #SBATCH -e curl.error
@@ -18,3 +18,14 @@ cd /home/s4642506/RL-Visual-Continuous-Control
 
 srun python ./src/train.py --agent curl
 # srun python ./src/test.py --agent sacae
+
+
+# --domain_name finger --task_name spin
+# --domain_name reacher --task_name easy
+# --domain_name cartpole --task_name swingup
+# --domain_name cheetah --task_name run
+# --domain_name walker --task_name walk
+# --domain_name ball_in_cup --task_name catch
+
+
+#Finger Spin, Cartpole Swingup, Reacher Easy, Cheetah Run, Walker Walk, and Ball in Cup Catch
